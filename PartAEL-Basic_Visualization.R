f997e75808ec8667b10bc644e35ce14f2b3cd165
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-06-07 15:16:40 -0500

    Alex's changes

--- PartAEL-Basic_Visualization.R
+++ PartAEL-Basic_Visualization.R
@@ -1,4 +1,4 @@
-# Read in "fungicide.tidy"
+# Read in "fungicide.tidy" 
 fungicide.tidy <- read.csv("data/FungicideTidy.csv", head = TRUE)
 
 # Make a simple X-Y graph
@@ -10,7 +10,6 @@ plot(fungicide.tidy$Severity~fungicide.tidy$Julian.Date, xlab = "Julian Date", y
      cex.lab = 1.5, cex.main = 2)
 
 # Make a simple X-Y graph, where the symbols differ for "control" and "fungicide"
-# Alex: fix this, get this to work, and add a legend
 color.vector <- rep("purple", nrow(fungicide.tidy))
 color.vector[which(fungicide.tidy$Experiment == "control" )] <- "red"
 
@@ -23,35 +22,36 @@ plot(fungicide.tidy$Severity~fungicide.tidy$Julian.Date, xlab = "Julian Date", y
 legend("topleft", col = c("purple", "red"), pch = c(25,24), legend = c("Fungicide", "Control"))
 
 
+#Sort the data by severity. This will help make some of the plots below prettier.
+fungicide.tidy <- fungicide.tidy[order(fungicide.tidy$Severity),]
+
 # Show how to have two side-by-side plots
 fungicide.tidy.fungicide <- fungicide.tidy[which(fungicide.tidy$Experiment != "control" ),]
 fungicide.tidy.control <- fungicide.tidy[which(fungicide.tidy$Experiment == "control" ),]
-pdf("results/Disease.Severity.Fungicide.vs.Control.pdf", width = 12)
-  par(mfrow = c(1,2), mar = c(5,5,5,5), lab = c(5,5,7))
-  plot(fungicide.tidy.fungicide$Severity~fungicide.tidy.fungicide$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Fungicide",
-       cex.lab = 1.5, cex.main = 2, col = "purple", pch = 25)
-  plot(fungicide.tidy.control$Severity~fungicide.tidy.control$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Control",
-       cex.lab = 1.5, cex.main = 2, col = "red", pch = 24)
-dev.off()
+par(mfrow = c(1,2), mar = c(2,2,2,2), lab = c(5,5,7))
+plot(fungicide.tidy.fungicide$Severity~fungicide.tidy.fungicide$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Fungicide",
+     cex.lab = 1.5, cex.main = 2, col = "purple", pch = 25)
+plot(fungicide.tidy.control$Severity~fungicide.tidy.control$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Control",
+     cex.lab = 1.5, cex.main = 2, col = "red", pch = 24)
+
 # Show the other visual ways things can be plotted (e.g., dots, versus lines connecting the dots versus both versus horizontal lines)
 
+
+
 # Change the symbols to lines
-pdf("results/Disease.Severity.Fungicide.vs.Control.lines.pdf", width = 12)
-  par(mfrow = c(1,2), mar = c(5,5,5,5), lab = c(5,5,7))
-  plot(fungicide.tidy.fungicide$Severity~fungicide.tidy.fungicide$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Fungicide",
-       cex.lab = 1.5, cex.main = 2, col = "purple", pch = 25, type = "l")
-  plot(fungicide.tidy.control$Severity~fungicide.tidy.control$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Control",
-       cex.lab = 1.5, cex.main = 2, col = "red", pch = 24, type = "l")
-dev.off()
+par(mfrow = c(1,2), mar = c(2,2,2,2), lab = c(5,5,7))
+plot(fungicide.tidy.fungicide$Severity~fungicide.tidy.fungicide$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Fungicide",
+     cex.lab = 1.5, cex.main = 2, col = "purple", pch = 25, type = "l")
+plot(fungicide.tidy.control$Severity~fungicide.tidy.control$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Control",
+     cex.lab = 1.5, cex.main = 2, col = "red", pch = 24, type = "l")
+
 
 # Change the symbols to  points and lines
-pdf("results/Disease.Severity.Fungicide.vs.Control.points.and.lines.pdf", width = 12)
-  par(mfrow = c(1,2), mar = c(5,5,5,5), lab = c(5,5,7))
-  plot(fungicide.tidy.fungicide$Severity~fungicide.tidy.fungicide$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Fungicide",
-       cex.lab = 1.5, cex.main = 2, col = "purple", pch = 25, type = "b")
-  plot(fungicide.tidy.control$Severity~fungicide.tidy.control$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Control",
-       cex.lab = 1.5, cex.main = 2, col = "red", pch = 24, type = "b")
-dev.off()
+par(mfrow = c(1,2), mar = c(2,2,2,2), lab = c(5,5,7))
+plot(fungicide.tidy.fungicide$Severity~fungicide.tidy.fungicide$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Fungicide",
+     cex.lab = 1.5, cex.main = 2, col = "purple", pch = 25, type = "b")
+plot(fungicide.tidy.control$Severity~fungicide.tidy.control$Julian.Date, xlab = "Julian Date", ylab = "Disease Severity", main = "Control",
+     cex.lab = 1.5, cex.main = 2, col = "red", pch = 24, type = "b")
 
 # Change the symbols so that they are "histogram-like"
 pdf("results/Disease.Severity.Fungicide.vs.Control.histogram.like.pdf", width = 12)
@@ -62,6 +62,8 @@ pdf("results/Disease.Severity.Fungicide.vs.Control.histogram.like.pdf", width = 
        cex.lab = 1.5, cex.main = 2, col = "red", pch = 24, type = "h")
 dev.off()
 
+#Reset par at the end so that none of the bells and whistles specified in previous graphs are carried foward to the next graph
+par()
 
 
 
