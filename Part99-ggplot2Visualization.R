d3db4746299a6107e60e61b870826900467ed813
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-06-06 23:10:52 -0500

    Rewrite parts 2 and 3

--- Part99-ggplot2Visualization.R
+++ Part99-ggplot2Visualization.R
@@ -1,6 +1,6 @@
 # 
-# Part 3: Data Transformation and Plotting
-# ========================================
+# Part 3: Plotting with ggplot2
+# =============================
 # 
 # So far we have covered:
 # 
@@ -10,6 +10,7 @@
 #  - reading documentation
 #  - using functions
 #  - saving data
+#  - reshaping data
 #  
 # Of course, we haven't used one of R's most powerful assets: graphics. This 
 # section is dedicated to creating a plot from the data. While R has very
@@ -30,8 +31,7 @@
 # After this section, you should have the tools to:
 # 
 #  1. create a simple plot in ggplot2
-#  2. reshape data (aka pivot and unpivot)
-#  3. save plots
+#  2. save plots
 # 
 # Again, since this is a three hour workshop, we do not expect mastery, but this
 # at least should give you a starting point. With that in mind, let's get 
@@ -40,17 +40,8 @@
 # Packages
 # --------
 # 
-# For the tasks in this example, we will load the "tidyverse" package. This will
-# load six packages (see https://tidyverse.tidyverse.org):
-# 
-#  - ggplot2, for data visualisation.
-#  - dplyr, for data manipulation.
-#  - tidyr, for data tidying.
-#  - readr, for data import.
-#  - purrr, for functional programming.
-#  - tibble, for tibbles, a modern re-imagining of data frames.
-# 
-# We will be using the *ggplot2* and *tidyr* packages 
+# We will be using the *ggplot2* and *readr* packages, both of which are in the
+# tidyverse.
 
 library("tidyverse")
 
@@ -63,187 +54,9 @@ library("tidyverse")
 # conversion of characters (strings) to factors, provides better error messages,
 # and is generally more efficient. 
 
-fungicide <- read_csv("data/FungicideExample.csv")
-fungicide
-
-# Note that this looks slightly different than the data frame we saw earlier.
-# Never fear, this is still a data frame. We can confirm this with 
-# `is.data.frame()`:
-
-is.data.frame(fungicide)
-
-# It's also a "tibble", which is a form of data frame that gives more
-# information about your data (e.g. what kind of data the columns are). 
-# 
-# Reshaping Data
-# --------------
-# 
-# At some point in time, you will need to reshape/rearrange data in R. This is
-# a fact of life. Doing this in R is advantageous for three reasons:
-# 
-#  1. your original data is in tact
-#  2. you have a trail of self-documenting changes
-#  3. your future self will tank you
-# 
-# In order to use our data with ggplot2, we need to make sure it is in a "tidy"
-# form where we have one observation per row. We currently have six observations
-# per row. Thus, we will need to reshape this data from this:
-# 
-# ```
-#   Julian.Date TwentyOneThirtySevenWheat TwentyOneThirtySevenWheat.trt CutterWheat CutterWheat.Trt JaggerWheat JaggerWheat.Trt
-# 1         139                      2.34                          1.81        1.15            1.79        1.85            2.27
-# 2         146                      7.56                          7.89        3.62            2.40        6.92            5.00
-# 3         154                     28.78                         15.04       17.89            6.21       47.39           20.17
-# ```
-# 
-# to this:
-# 
-# ```
-#   Experiment Julian.Date                  Cultivar Severity
-# 1    control         139 TwentyOneThirtySevenWheat     2.34
-# 2    control         146 TwentyOneThirtySevenWheat     7.56
-# 3    control         154 TwentyOneThirtySevenWheat    28.78
-# 4    control         139               CutterWheat     1.15
-# 5    control         146               CutterWheat     3.62
-# 6    control         154               CutterWheat    17.89
-# 7    control         139               JaggerWheat     1.85
-# 8    control         146               JaggerWheat     6.92
-# 9    control         154               JaggerWheat    47.39
-# 10 treatment         139 TwentyOneThirtySevenWheat     1.81
-# 11 treatment         146 TwentyOneThirtySevenWheat     7.89
-# 12 treatment         154 TwentyOneThirtySevenWheat    15.04
-# 13 treatment         139               CutterWheat     1.79
-# 14 treatment         146               CutterWheat     2.40
-# 15 treatment         154               CutterWheat     6.21
-# 16 treatment         139               JaggerWheat     2.27
-# 17 treatment         146               JaggerWheat     5.00
-# 18 treatment         154               JaggerWheat    20.17
-# ```
-
-stop("
-      Discussion: What are the differences between the two data frames above?
-      How can we get from one data frame to the other?
-    ")
-
-# 
-# There are several ways of creating the tidy data frame; we will show one here,
-# but there are others that we will discuss during the workshop. 
-# 
-# The process we will demonstrate takes three steps:
-#  
-#  1. subset control and treatment data into separate data frames
-#  2. collect each record into a single row with the *tidyr* function `gather()`
-#     (This is also known as "unpivot" in Excel: 
-#     http://www.excel-university.com/unpivot-excel-data/).
-#  3. combine the two data frames and create a new column called "Experiment"
-#     with the *dplyr* function `bind_rows()`
-# 
-# ### Step 1: Subset data
-# 
-# Subset control and treatmend data into separate data frames. Since we have a
-# data frame, we will use matrix-like subsetting.
-
-control   <- fungicide[, c(1, 2, 4, 6)]
-treatment <- fungicide[, c(1, 3, 5, 7)]
-
-# Now we have two separate data frames with 4 columns each, but still we have
-# three observations per row. 
-
-stop("
-      Would this approach work for a different data set? Why or why not?
-     ")
-
-# ### Step 2: Tidy data
-# 
-# Now that we have our experiments in separate tables, we can reshape each 
-# table into tidy format by using *tidyr*'s `gather()`. This will "gather" all
-# of the data that are spread across columns into single rows. Users of excel
-# might be familiar with this operation as "unpivot". 
-# 
-# In order to do this, we will need to specify three things:
-# 
-#  - A new column name to specify where the data came from (key)
-#  - A new column name to specify what the data represents (value)
-#  - The columns to include (or exclude) 
-# 
-# I often find the key/value syntax confusing and have to look at the examples
-# in the help page for `gather()`.
-# 
-
-stop("
-      Look at the examples for gather and determine what the names of the
-      key and value columns should be for our data.
-      (hint: you can use example(gather) to run them automatically).
-    ")
-
-# 
-# 
-# 
-# Now that we have our keys and values, we can plug them into `gather()`. 
-# 
-# > Note, however, this uses "bare" names (names that are not between quotation
-# > marks) to specify columns. This is a feature of the tidyverse to make typing
-# > easier, but does not necessarily work outside of the tidyverse. 
-# 
-# To avoid typing all of the columns we want to collapse, we'll simply specify
-# the column that we don't want to collapse, which is Julian.Date. This is done
-# by placing a minus (-) symbol in front of it.
-
-con.tidy <- gather(data = control, key = Cultivar, value = Severity, -Julian.Date)
-con.tidy
-
-# Note that we can easily get back our original data by using the `spread()`
-# function, which is the opposite of `gather()`:
-
-spread(data = con.tidy, key = Cultivar, value = Severity)
-
-# This was simply to demonstrate that with tidy data, it's possible to reshape
-# it in most any way possible.
-# 
-# Now we can tidy our treatment data. But first, because we want to combine this
-# data with our control, we want to make sure that the values in the "Cultivar"
-# column are the same. Currently we have the following columns in our treatment
-# data.
-
-colnames(treatment)
-
-# If we take a look at these column names, we can see that they all end in .Trt,
-# except for the second column, which ends in .trt. Because R is case-sensitive,
-# it will see .trt and .Trt as two different things. To avoid this problem, we
-# will simply replace the column names of the treatment to those of the control.
-
-colnames(control)
-colnames(treatment) <- colnames(control)
-treatment
-
-# Now we can tidy our treatment data.
-
-trt.tidy <- gather(data = treatment, key = Cultivar, value = Severity, -Julian.Date)
-
-# ### Step 3: combining experiments
-# 
-# We can combine the treatments by using the *dplyr* function `bind_rows()`, 
-# which takes any number of data frames and places them on top of one another.
-# It takes an optional argument, `.id` (note the ".") that specifies a separate
-# column to create identifying the source data frame.
-
-fungicide.tidy <- bind_rows(control = con.tidy, fungicide = trt.tidy, .id = "Experiment")
+fungicide.tidy <- read_csv("data/FungicideTidy.csv")
 fungicide.tidy
 
-# 
-# 
-# We've gone to the work to tidy up the data; let's save it to our disk so that
-# we can easily reuse it later.
-# 
-
-write_csv(fungicide.tidy, path = "data/FungicideTidy.csv")
-
-# We have our data in a tidy format so we can now use this to create a
-# visualization using ggplot2.
-# 
-# 
-# 
-# 
 # Visualizing Data
 # ----------------
 # 
